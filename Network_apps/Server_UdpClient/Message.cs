72f8a1620c2270acf56ccd72f74aaf21d7d68d9a
Aleksandr Ushenin <sasha0625@mail.ru>
2023-12-03 10:35:43 +0300

    Урок 1. Работа с сетью: чтение и запись данных в сеть. Клиентские и серверные приложения

---
+++ Network_apps/Server_UdpClient/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_UdpClient
+{
+    public class Message
+    {
+        public uint Id { get; set; }
+        public string MessageText { get; set; }
+        public string SenderIp { get; set; }
+        public int SenderPort { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}  SenderIp: {SenderIp}  SenderPort: {SenderPort}\nMessageText: {MessageText}";
+        }
+    }
+}
