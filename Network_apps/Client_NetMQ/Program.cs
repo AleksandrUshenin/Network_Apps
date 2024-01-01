c5d93b6bba24a4fc71c33454d6cec325ac07974b
Aleksandr Ushenin <sasha0625@mail.ru>
2024-01-01 17:17:00 +0300

    Урок 7.
    Библиотеки: Nuget и разработка собственных библиотек

---
+++ Network_apps/Client_NetMQ/Program.cs
@@ -0,0 +1,13 @@
+﻿using Server_UdpClient;
+
+namespace Client_NetMQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client_NET_MQ client = new Client_NET_MQ("127.0.0.1", 12345, new ConsoleUserInterface());
+            client.Run();
+        }
+    }
+}
---
+++ Network_apps/Server_NetMQ/Program.cs
@@ -0,0 +1,13 @@
+﻿using Server_UdpClient;
+
+namespace Server_NetMQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server_NET_MQ server = new Server_NET_MQ("127.0.0.1", 12345, new ConsoleUserInterface());
+            server.Run();
+        }
+    }
+}
